9cc020db0cec27b0e3e598de06d48bd65fc072e2
Sebastián Ramírez <tiangolo@gmail.com>
2015-09-28 12:27:00 -0500

    Add m4a to ingestable audio files

--- ingest.sh
+++ ingest.sh
@@ -2,7 +2,7 @@
 
 if [[ -d "/audio" ]]
 then
-  find /audio -name "*.mp3" -o -name "*.aac" -o -name "*.wav" > /audio/audio_to_ingest
+  find /audio -name "*.mp3" -o -name "*.aac" -o -name "*.wav" -o -name "*.m4a" > /audio/audio_to_ingest
   echoprint-codegen -s < /audio/audio_to_ingest > /audio/audiocodes.json
   cd $ECHOPRINT_PATH/echoprint-server/util
   python fastingest.py /audio/audiocodes.json
